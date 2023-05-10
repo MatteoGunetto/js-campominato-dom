8f32bf72b437465d4b9f82fc3de58bc7d7090e6f
MatteoGunetto <Matteogunetto@tiscali.it>
2023-05-10 15:15:30 +0200

    random number generator

--- jsgrid.js
+++ jsgrid.js
@@ -4,10 +4,24 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+// Consegna PARTE 2     
+
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+
 //selezion elementi nel DOM
 const elementInfo = document.querySelector('.info');
 const elementGrid = document.querySelector ('.grid');
 const buttonPlay = document.querySelector('#play');
+let n_Cells = 100;
+let arrMines;
+let score;
+
 
 // Istruzioni bottone
 buttonPlay.addEventListener('click', function(){
@@ -18,17 +32,21 @@ buttonPlay.addEventListener('click', function(){
     //Appare contorno griglia
     elementGrid.classList.remove('hidden');
 
-    //Numero celle 
-    const n_Cells = 100;
-
     //Genero griglia completa
     createGrid(n_Cells, elementGrid);
 
-    //Reset schermata griglia 
- elementGrid.innerHtml = '';
+    // generare array con le bombe
+    arrMines = generateRandomArray(1, n_Cells, 16);
+    console.log(arrMines);
+
+    // inizializziamo il punteggio
+    score = 0;
+    console.log(score);
+
+
+
 });
 
- 
 
 // FUNZIONI
 function createGrid (n_Cells, elementContainer){
@@ -52,3 +70,24 @@ function createGrid (n_Cells, elementContainer){
         });
     }
 }
+// Array Random Num generator
+function getRandomNum(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function generateRandomArray(min, max, nElements) {
+	let arr = [];
+	for (let i = 0; i < nElements; i++) {
+
+		let randomNum;
+		do {
+			randomNum = getRandomNum(min, max);
+		} while (arr.includes(randomNum))
+
+		arr.push(randomNum);
+	}
+
+	return arr;
+}
+
+
